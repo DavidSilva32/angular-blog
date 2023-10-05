4fe74a97e7e9b3fc099fc9c2869e031a0022386e
David Teixeira <118708351+DavidSilva32@users.noreply.github.com>
2023-10-05 17:56:41 -0300

    Components and styles created using Mobile First

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.sass']
+})
+export class BigCardComponent implements OnInit {
+  image: string = "https://th.bing.com/th/id/R.3e77a1db6bb25f0feb27c95e05a7bc57?rik=DswMYVRRQEHbjQ&riu=http%3a%2f%2fwww.coalitionrc.com%2fwp-content%2fuploads%2f2017%2f01%2fplaceholder.jpg&ehk=AbGRPPcgHhziWn1sygs8UIL6XIb1HLfHjgPyljdQrDY%3d&risl=&pid=ImgRaw&r=0"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
