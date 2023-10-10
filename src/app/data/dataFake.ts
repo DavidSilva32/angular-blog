f73ccbc711d5e59ae90523207e6e10cc939231bc
David Teixeira <118708351+DavidSilva32@users.noreply.github.com>
2023-10-10 18:58:49 -0300

    update content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,26 @@
 export const dataFake = [
     {
         "id": "1",
-        "photoCover": "https://th.bing.com/th/id/OIP.ycDC9N1rUlha8ViOS4DemQHaEK?pid=ImgDet&rs=1",
         "title": "New Movie",
-        "description": "Hello"
+        "photoCover": "https://th.bing.com/th/id/OIP.ycDC9N1rUlha8ViOS4DemQHaEK?pid=ImgDet&rs=1",
+        "description": "Iron Man is the superhero persona of Anthony Edward Tony Stark, a businessman and engineer who runs the company Stark Industries. Beginning his career as a weapons manufacturer, he is captured in a war zone, and his heart is severely injured by shrapnel. To sustain his heart and escape his captors, he builds a technologically advanced armor. After escaping, he continues using the armor as a superhero, creating more advanced models that grant him superhuman strength, flight, energy projection, and other abilities."
     },
     {
         "id": "2",
-        "photoCover": "https://th.bing.com/th/id/R.22bdd8200d5a36d59641b65f31f57e41?rik=xQPJzamstpKY1A&pid=ImgRaw&r=0",
         "title": "New Serie announced of the Disney +",
-        "description": "Hello"
+        "photoCover": "https://th.bing.com/th/id/R.22bdd8200d5a36d59641b65f31f57e41?rik=xQPJzamstpKY1A&pid=ImgRaw&r=0",
+        "description": "Black Panther is a 2018 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 18th film in the Marvel Cinematic Universe (MCU)."
     },
     {
         "id": "3",
+        "title": "New Marvel's movie announced, bringing The Root Hulk",
         "photoCover": "https://th.bing.com/th/id/R.e6c71cc7983e7e052c70974cf2f36f8c?rik=W3%2fv7cU9aIfNKw&riu=http%3a%2f%2fcdn.collider.com%2fwp-content%2fuploads%2f2019%2f04%2fthe-incredible-hulk.jpg&ehk=P0Cw5lBrn%2baBMM6XrAOBcOf%2b%2b%2b31MHKVrmctlr8cetM%3d&risl=&pid=ImgRaw&r=0",
-        "title": "New Marvel's movie announced, briging The Root Hulk",
-        "description": "Hello"
+        "description": "The Incredible Hulk is a fictional superhero who appears in comic books, movies, and TV shows. He is the alter ego of Dr. Bruce Banner, a scientist who was exposed to gamma radiation and gained the ability to transform into a green-skinned giant with superhuman strength and durability when he is angry or stressed. The Hulk is one of the most popular characters in the Marvel Universe and has been portrayed by actors such as Lou Ferrigno, Eric Bana, Edward Norton, and Mark Ruffalo . He is also known for his catchphrase “Hulk smash!” and his rivalry with other heroes such as Thor, Iron Man, and Wolverine."
     },
     {
         "id": "4",
-        "photoCover": "https://th.bing.com/th/id/R.59fc1adf2d7aae758374430d7a5c6b20?rik=5vHKe4LN1Or9MQ&pid=ImgRaw&r=0",
         "title": "New Loki's serie",
-        "description": "Hello"
+        "photoCover": "https://th.bing.com/th/id/R.59fc1adf2d7aae758374430d7a5c6b20?rik=5vHKe4LN1Or9MQ&pid=ImgRaw&r=0",
+        "description": "Loki is a complex and fascinating character from Norse mythology and Marvel comics. He is the god of mischief, lies, and trickery, as well as the adopted son of Odin and the brother of Thor. He is known for his cunning, intelligence, shape-shifting, and magic abilities. He often causes trouble for the gods and the heroes of the Marvel Cinematic Universe (MCU), but sometimes he also helps them out of his own self-interest or redemption."
     },
 ]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from '../../data/dataFake'
+import { Article } from 'src/app/models/article';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  smallCard_list: Article
 
-  constructor() { }
-
-  ngOnInit(): void { 
+  constructor() { 
+    this.smallCard_list = [{
+      id: '',
+      title: '',
+      photoCover: '',
+      description: '',
+    }]
   }
 
+  ngOnInit(): void { 
+    this.getSmallCardData('1')
+  }
+
+  getSmallCardData(id: string) {
+    const result = dataFake.filter((article) => article.id != id)
+
+    this.smallCard_list = result
+  }
 }
